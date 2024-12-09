f8e97fe53720bb79598bb47e509804d3a024c5c7
= <=>
2024-12-09 06:24:09 -0800

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const db = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${db}`;
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
